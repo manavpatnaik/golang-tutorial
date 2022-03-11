11d21f8e371d98a2f74f1a643f6c080cd23da610
Manav Patnaik <dev.manavpatnaik@gmail.com>
2022-03-11 06:44:22 +0530

    Variables

--- 1_variables.go
+++ 1_variables.go
@@ -23,4 +23,21 @@ func main() {
 	var ageTwo = 30
 	ageThree := 40
 	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// Variations of the int - bits and memory
+	var numOne int8 = 25 // Range: [-2^7, 2^7-1] i.e [-128, 127]
+	var numTwo int16 = 26
+	var numThree = 129
+	var numFour uint = 12
+	var numFive uint8 = 13
+	fmt.Println(numOne, numTwo, numThree)
+	fmt.Println(numFour, numFive)
+
+	// Floats
+	var scoreOne float32 = 24
+	var scoreTwo float64 = 24234324 // Better to use float64, higher precision
+	scoreThree := 3.4               // By default floats will be inferred at float64
+	fmt.Println(scoreOne, scoreTwo, scoreThree)
+
+	// Reference: https://go.dev/ref/spec#Numeric_types
 }
